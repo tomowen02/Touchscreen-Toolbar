928c6dc273df30a10c88eaf81b2ddf122615a901
Tom Owen <tom@tomiowen.com>
2022-11-14 20:01:43 +0000

    Clean-up code

--- commands.py
+++ commands.py
@@ -1,4 +1,4 @@
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller as Keyboard_Controller
 from pynput.mouse import Controller as Mouse_Controller
 from os import system
 from time import sleep
@@ -6,6 +6,7 @@ from popupmsg import popupmsg
 from tempfile import gettempdir
 
 mouse = Mouse_Controller()
+keyboard = Keyboard_Controller()
 
 ### Functions to help with other commands ###
 
@@ -21,7 +22,6 @@ def get_cursor_pos(filename="pointer_loc.txt"):
 
 def snip_screen():
     pos_before_snip = get_cursor_pos()
-    keyboard = Controller()
     keyboard.press(Key.cmd)
     keyboard.press(Key.shift)
     keyboard.press("s")
@@ -32,21 +32,18 @@ def snip_screen():
     
 def hold_shift():
     popupmsg("The shift key is about to be held down virtually")
-    keyboard = Controller()
     keyboard.press(Key.shift)
     sleep(5)
     keyboard.release(Key.shift)
     popupmsg("DONE!")
 
 def multi_task():
-    keyboard = Controller()
     keyboard.press(Key.cmd)
     keyboard.press(Key.tab)
     keyboard.release(Key.cmd)
     keyboard.release(Key.tab)
     
 def win():
-    keyboard = Controller()
     keyboard.press(Key.cmd)
     keyboard.release(Key.cmd)
 
@@ -57,7 +54,6 @@ def whiteboard():
     system("start ms-whiteboard-cmd:")
 
 def copy():
-    keyboard = Controller()
     keyboard.press(Key.alt_l)
     keyboard.press(Key.tab)
     sleep(0.1)
@@ -71,7 +67,6 @@ def copy():
     print("done")
     
 def paste():
-    keyboard = Controller()
     keyboard.press(Key.alt_l)
     keyboard.press(Key.tab)
     sleep(0.1)
@@ -85,7 +80,6 @@ def paste():
     print("done")
     
 def delete():
-    keyboard = Controller()
     keyboard.press(Key.alt_l)
     keyboard.press(Key.tab)
     sleep(0.1)
@@ -97,7 +91,6 @@ def delete():
     print("done")
     
 def osk():
-    keyboard = Controller()
     keyboard.press(Key.cmd)
     keyboard.press(Key.ctrl_l)
     keyboard.press("o")
